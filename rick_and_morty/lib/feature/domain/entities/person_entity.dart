79334b6f15d123cb6a4f558e7615b322556eb787
painedViktor <mudritskiy.vk@gmail.com>
2022-12-08 11:26:31 +0200

    domain level created

---
+++ rick_and_morty/lib/feature/domain/entities/person_entity.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+
+class PersonEntity extends Equatable {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
+  final LocationEntity origin;
+  final LocationEntity location;
+  final String image;
+  final List<String> episodes;
+  final DateTime created;
+  const PersonEntity({
+    required this.created,
+    required this.episodes,
+    required this.gender,
+    required this.id,
+    required this.image,
+    required this.location,
+    required this.name,
+    required this.origin,
+    required this.species,
+    required this.status,
+    required this.type,
+  });
+
+  @override
+  List<Object?> get props => [
+        created,
+        episodes,
+        gender,
+        id,
+        image,
+        location,
+        name,
+        origin,
+        species,
+        status,
+        type,
+      ];
+}
+
+class LocationEntity {
+  final String name;
+  final String url;
+  const LocationEntity({required this.name, required this.url});
+}
